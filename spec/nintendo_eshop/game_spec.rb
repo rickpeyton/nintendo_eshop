0efc9f67303b1c95bbe59a27ad93c2db4d971cf4
Rick Peyton <peytorb@gmail.com>
2019-07-04 12:01:30 -0500

    Update README

--- spec/nintendo_eshop/game_spec.rb
+++ spec/nintendo_eshop/game_spec.rb
@@ -23,25 +23,25 @@ RSpec.describe NintendoEshop::Game do
       expect(game.sale_price).to eq nil
     end
 
+    it "retrieves a game via the object id" do
+      game = NintendoEshop::Game.retrieve_by(object_id: "26851cb3-4f10-333a-8d7c-f9cae4a9bc03")
+
+      expect(game).to be_a NintendoEshop::Game
+      expect(game.art).to match(/Switch_BridgeConstructorPortal_box_eShop.png/)
+      expect(game.current_price).to eq 7.49
+      expect(game.id).to eq "70010000001023"
+      expect(game.msrp).to eq 14.99
+      expect(game.object_id).to eq "26851cb3-4f10-333a-8d7c-f9cae4a9bc03"
+      expect(game.platform).to eq "Nintendo Switch"
+      expect(game.sale_price).to eq 7.49
+      expect(game.title).to eq "Bridge Constructor Portal"
+      expect(game.url).to eq "/games/detail/bridge-constructor-portal-switch"
+    end
+
     it "raises an invalid request if a game is not found" do
       game = -> { NintendoEshop::Game.retrieve_by(id: "invalid") }
 
       expect { game.call }.to raise_error(NintendoEshop::InvalidRequestError, "ID not found")
     end
-  end
-
-  it "retrieves a game via the object id" do
-    game = NintendoEshop::Game.retrieve_by(object_id: "26851cb3-4f10-333a-8d7c-f9cae4a9bc03")
-
-    expect(game).to be_a NintendoEshop::Game
-    expect(game.art).to match(/Switch_BridgeConstructorPortal_box_eShop.png/)
-    expect(game.current_price).to eq 7.49
-    expect(game.id).to eq "70010000001023"
-    expect(game.msrp).to eq 14.99
-    expect(game.object_id).to eq "26851cb3-4f10-333a-8d7c-f9cae4a9bc03"
-    expect(game.platform).to eq "Nintendo Switch"
-    expect(game.sale_price).to eq 7.49
-    expect(game.title).to eq "Bridge Constructor Portal"
-    expect(game.url).to eq "/games/detail/bridge-constructor-portal-switch"
   end
 end
